bf122fd371ec455e96d1b1e360de0fb9615910d2
Rafadsaol <Rafadsaol@outlook.com>
2024-05-29 13:57:00 -0300

    exercicios

---
+++ iniciante/aula1/exercicios/treino.py
@@ -0,0 +1,11 @@
+ganhos = 1000
+vendas = 800
+sobras = ganhos - vendas
+total = ganhos + vendas
+
+print("O total de ganhos foi de", total,"já a quantidade de sobras foi de", sobras)
+
+if total > 1000:
+    print("Parabéns, ganhou um bônus!")
+else:
+    print("Não ganhou bônus...")
